0a1ca0254e9d22d9bad92bf919dcd3fcb158fcc2
mbk2016 <mateuszblaszczyk94@gmail.com>
2016-05-09 00:26:52 +0200

    Fixed finding winning ticket
    Also added user interface

--- Lottery/Program.cs
+++ Lottery/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,12 @@ namespace Lottery
         {
             Coupon ListOfCoupons = new Coupon();
             List<int> winningCoupon = new List<int>(6) { 4, 6, 16, 17, 21, 28 };
-            ListOfCoupons.AddCoupons(20);
-            //ListOfCoupons.CheckWinner(winningCoupon);
-
+            ListOfCoupons.AddCoupons(50000000);
+            if (ListOfCoupons.CheckWinner(winningCoupon))
+            {
+                Console.WriteLine("You won!");
+            }
+            else Console.WriteLine("You did not win.");
             Console.ReadKey();
         }
     }
